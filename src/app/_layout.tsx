4477ca156b6a7a1faa312b9cdc8b57b06748484e
Nana Mohamadi <adilnana78@gmail.com>
2026-03-26 10:56:48 +0000

    feat: implement authentication flow with Clerk and social login options

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,9 +1,34 @@
-import { Stack } from "expo-router"
+import { Stack } from "expo-router";
+import { ClerkProvider } from "@clerk/expo";
+import { tokenCache } from "@clerk/expo/token-cache";
 
-const RootLayout = () => {
-  return (
-    <Stack screenOptions={{ headerShown: false }} />
-  )
+import "../../global.css";
+import {
+  ThemeProvider,
+  DarkTheme,
+  DefaultTheme,
+} from "@react-navigation/native";
+import { useColorScheme } from "react-native";
+import { KeyboardProvider } from "react-native-keyboard-controller";
+
+const publishableKey = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY!;
+
+if (!publishableKey) {
+  throw new Error("Add your Clerk Publishable Key to the .env file");
 }
 
-export default RootLayout
+const RootLayout = () => {
+  const colorScheme = useColorScheme();
+
+  return (
+    <ClerkProvider publishableKey={publishableKey} tokenCache={tokenCache}>
+      <KeyboardProvider>
+        <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
+          <Stack screenOptions={{ headerShown: false }} />
+        </ThemeProvider>
+      </KeyboardProvider>
+    </ClerkProvider>
+  );
+};
+
+export default RootLayout;
