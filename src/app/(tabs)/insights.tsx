f876be283a576dd958d29fa0937711394be774d4
Nana Mohamadi <adilnana78@gmail.com>
2026-04-03 17:54:05 +0000

    feat: add List, Planner, and Insights screens with tab navigation layout

---
+++ src/app/(tabs)/insights.tsx
@@ -0,0 +1,11 @@
+import { View, Text } from 'react-native'
+
+const InsightsScreen = () => {
+  return (
+    <View>
+      <Text>InsightsScreen</Text>
+    </View>
+  )
+}
+
+export default InsightsScreen
